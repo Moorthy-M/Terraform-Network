621def576c5962c76f4dbc84b78d1559333135cc
Moorthy-M <moorthy2652002@gmail.com>
2025-12-25 13:25:08 +0000

    VPC release version Update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/Moorthy-M/Terraform-Modules.git//vpc?ref=vpc-v1.0"
+  source = "git::https://github.com/Moorthy-M/Terraform-Modules.git//vpc?ref=vpc-v1.1"
 
   vpc_cidr        = var.vpc_cidr
   public_subnets  = var.public_subnets
